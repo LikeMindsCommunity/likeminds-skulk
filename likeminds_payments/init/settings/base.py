c2fde5536b846c57e53fac6a387bbcf53b0f8119
Mahir-G2 <mahir.gupta@likeminds.community>
2021-06-09 18:52:31 +0530

    handled cors

--- likeminds_payments/init/settings/base.py
+++ likeminds_payments/init/settings/base.py
@@ -36,10 +36,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'subscription'
+    'subscription',
+    'corsheaders'
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

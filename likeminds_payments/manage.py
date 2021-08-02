dcc4a8e466000223806c5c31cf2de4905e15bac3
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2021-08-02 13:28:48 +0530

    adding coralogix fix

--- likeminds_payments/manage.py
+++ likeminds_payments/manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'init.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'init.settings.development')
+    from coralogix.handlers import CoralogixLogger
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

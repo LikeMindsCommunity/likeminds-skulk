750f3807b0d68019685cf603fee78b8b6b44e836
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2021-08-20 16:24:27 +0530

    remove unecessary

--- likeminds_payments/init/settings/development.py
+++ likeminds_payments/init/settings/development.py
@@ -6,15 +6,11 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('BETA_DB_NAME'),
-        'USER': os.getenv('BETA_DB_USER'),
-        'PASSWORD': os.getenv('BETA_DB_PASSWORD'),
-        'HOST': os.getenv('BETA_DB_HOST'),
-        'PORT': '5432',
-        'CONN_MAX_AGE': 600
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 TIME_ZONE = 'Asia/Kolkata'
 

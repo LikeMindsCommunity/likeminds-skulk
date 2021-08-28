94fe263e6e91ecc9aa3de65e2bf723359a5f47a0
priyanshuBoss <priyanshu@Priyanshus-MacBook-Pro.local>
2021-08-28 16:00:33 +0530

    adding live=true

--- likeminds_payments/subscription/external_services/webflow/constants.py
+++ likeminds_payments/subscription/external_services/webflow/constants.py
@@ -1,4 +1,4 @@
 WEBFLOW_HOST = "https://api.webflow.com"
-WEBFLOW_CREATE_EVENT_PATH = "collections/%s/items?live=true"
+WEBFLOW_CREATE_EVENT_PATH = "collections/%s/items"
 
-WEBFLOW_UPDATE_EVENT_PATH = WEBFLOW_CREATE_EVENT_PATH + "/%s?live=true"
+WEBFLOW_UPDATE_EVENT_PATH = WEBFLOW_CREATE_EVENT_PATH + "/%s"

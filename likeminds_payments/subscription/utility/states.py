c0d5987f77987371e7585f23063ec719fb19d5bc
Deepanshu Saxena <deepanshu.saxena@likeminds.community>
2021-10-18 15:36:37 +0530

    adding cohort types

--- likeminds_payments/subscription/utility/states.py
+++ likeminds_payments/subscription/utility/states.py
@@ -14,3 +14,16 @@ class MemberState:
     MEMBER = 4
     PROFILE_UNAVAILABLE = 9
     PENDING_MEMBER = 3
+
+
+class CohortTypes:
+    NORMAL = 0
+    SUBSCRIPTION_PLAN = 1
+    SUBSCRIPTION_EXPIRED_PLAN = 2
+    ALL_MEMBER = 3
+
+
+cohort_types = CohortTypes()
+
+cohort_type_list = [cohort_types.NORMAL, cohort_types.SUBSCRIPTION_PLAN,
+                    cohort_types.SUBSCRIPTION_EXPIRED_PLAN, cohort_types.ALL_MEMBER]

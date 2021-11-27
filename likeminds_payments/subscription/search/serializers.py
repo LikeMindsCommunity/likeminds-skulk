68545ffc02287ef17d8f0743ac10663c0d1eb25e
Deepanshu Saxena <deepanshu.saxena@likeminds.community>
2021-11-27 19:54:06 +0530

    Subscription Plan Document Serializer Added

---
+++ likeminds_payments/subscription/search/serializers.py
@@ -0,0 +1,26 @@
+from django_elasticsearch_dsl_drf.serializers import DocumentSerializer
+from .subscription_plan_index import SubscriptionPlanDocument
+
+
+class SubscriptionPlanDocumentSerializer(DocumentSerializer):
+    """Serializer for the Subscription Plan document."""
+
+    class Meta:
+        """Meta options."""
+
+        document = SubscriptionPlanDocument
+
+        fields = '__all__'
+
+    def to_representation(self, obj):
+        data = super(SubscriptionPlanDocumentSerializer, self).to_representation(obj)
+
+        fields = self._readable_fields
+
+        for field in fields:
+
+            if data[field.field_name] is None:
+                del data[field.field_name]
+
+        return data
+

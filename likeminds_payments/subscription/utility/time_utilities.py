03be0c4bc97b4766aa88ab4f009ee4fb0c6fa7e6
Deepanshu Saxena <deepanshu.saxena@likeminds.community>
2021-11-29 15:24:05 +0530

    added a function to convert epoch to MMM:YYYY format

--- likeminds_payments/subscription/utility/time_utilities.py
+++ likeminds_payments/subscription/utility/time_utilities.py
@@ -3,7 +3,6 @@ import time
 
 
 class TimeUtilities:
-
     MILLISECONDS_IN_A_DAY = 86400000
 
     DAYS_IN_A_WEEK = 7
@@ -111,3 +110,11 @@ class TimeUtilities:
             epoch_time = TimeUtilities.convert_milliseconds_to_sec(epoch_time)
 
         return time.strftime('%I:%M %p', time.localtime(epoch_time))
+
+    @staticmethod
+    def convert_epoch_to_month_year_format(epoch: int) -> str:
+
+        if TimeUtilities.is_epoch_in_milliseconds(epoch):
+            epoch = TimeUtilities.convert_milliseconds_to_sec(epoch)
+
+        return time.strftime('%b %Y', time.gmtime(epoch))

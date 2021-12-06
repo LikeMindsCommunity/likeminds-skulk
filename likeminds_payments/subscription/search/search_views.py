79bb1fbdebf75f357190c1c41a5886e9dd49f017
Deepanshu Saxena <deepanshu.saxena@likeminds.community>
2021-12-06 14:47:52 +0530

    used error_context

--- likeminds_payments/subscription/search/search_views.py
+++ likeminds_payments/subscription/search/search_views.py
@@ -58,13 +58,14 @@ class SearchHistoryView(APIView):
         page_size = RequestUtilities.get_page_size(request)
 
         if not member_id:
-            response = {'success': False, 'error_message': 'Invalid Member ID'}
+            response = ResponseUtilities.get_error_context(success=False, error_message="Invalid member_id in headers")
             return JsonResponse(response, status=status_codes.HTTP_403_FORBIDDEN)
 
         query_params = SearchViewHelper.get_validated_query_params(request)
 
         if query_params.get('error_message'):
-            response = {'success': False, 'error_message': query_params.get('error_message')}
+            response = ResponseUtilities.get_error_context(success=False,
+                                                           error_message=query_params.get('error_message'))
             return JsonResponse(response, status=status_codes.HTTP_400_BAD_REQUEST)
 
         search_manager = SearchImpl(member_id=member_id, search_term=query_params.get('search'),

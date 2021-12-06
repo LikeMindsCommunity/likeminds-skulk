37d448f6211a43c2c61b1a060d78445db41bbd93
Deepanshu Saxena <deepanshu.saxena@likeminds.community>
2021-12-06 11:22:22 +0530

    other minor changes

--- likeminds_payments/subscription/search/search_views.py
+++ likeminds_payments/subscription/search/search_views.py
@@ -7,6 +7,7 @@ from .search_impl import SearchImpl
 from .search_view_helper import SearchViewHelper
 
 from ..utility.request_utilities import RequestUtilities
+from ..utility.response_utilities import ResponseUtilities
 
 # ------------  do not remove this import ------------------
 
@@ -23,13 +24,14 @@ class SearchView(APIView):
         page_size = RequestUtilities.get_page_size(request)
 
         if not member_id:
-            response = {'success': False, 'error_message': 'Invalid Member ID'}
+            response = ResponseUtilities.get_error_context(success=False, error_message="Invalid member_id in headers")
             return JsonResponse(response, status=status_codes.HTTP_403_FORBIDDEN)
 
         query_params = SearchViewHelper.get_validated_query_params(request)
 
         if query_params.get('error_message'):
-            response = {'success': False, 'error_message': query_params.get('error_message')}
+            response = ResponseUtilities.get_error_context(success=False,
+                                                           error_message=query_params.get('error_message'))
             return JsonResponse(response, status=status_codes.HTTP_400_BAD_REQUEST)
 
         search_manager = SearchImpl(member_id=member_id, search_term=query_params.get('search'),

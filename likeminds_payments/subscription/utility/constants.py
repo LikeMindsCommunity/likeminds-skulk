d8c56e4cfe0f3699eebbbd593ff7cbd9f12de406
Naman Jain <89894541+naman-likeminds@users.noreply.github.com>
2021-12-10 19:10:43 +0530

    [LM-5614] added trigger for event creation mail (#143)
    * added trigger for event creation mail
    
    * made event_creation_mail task a celery one
    
    * changes after review
    
    * logging exception instead of printing it

--- likeminds_payments/subscription/utility/constants.py
+++ likeminds_payments/subscription/utility/constants.py
@@ -25,4 +25,7 @@ UPDATE_COHORT_API = "{}/api/cohort/update".format(settings.CORE_SERVICE_URL)
 PAYMENT_PAGE_BRANCH_URL = "{}/api/community/fetch_payment_page_url".format(settings.CORE_SERVICE_URL)
 CMS_USER_NAME = 'teamGrowth'
 CMS_PASSWORD = 'TheLMGrowth@1001'
+
+TRIGGER_EVENT_CREATION_MAIL = '{}/api/notifications/send_event_creation_mail'.format(settings.CORE_SERVICE_URL)
+
 ADMIN_EMAIL = 'admin@likeminds.community'

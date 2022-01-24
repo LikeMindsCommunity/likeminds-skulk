d291596e99c0457b7faa4276f505b4268afaea67
Deepanshu Saxena <deepanshu.saxena@likeminds.community>
2022-01-24 13:19:46 +0530

    used get_error_context

--- likeminds_payments/subscription/transactions/transaction_view_impl.py
+++ likeminds_payments/subscription/transactions/transaction_view_impl.py
@@ -307,7 +307,7 @@ class CreateFreeTransactionView(APIView):
 
         if 'error_message' in response_data:
             return JsonResponse(
-                {'success': False, 'error_message': response_data['error_message']},
+                ResponseUtilities.get_error_context(False, response_data['error_message']),
                 status=status_codes.HTTP_200_OK
             )
 

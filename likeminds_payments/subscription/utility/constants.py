014b9a4c41cf823546e46efb8eef1f953efcf0fd
Deepanshu Saxena <deepanshu.saxena@likeminds.community>
2022-02-10 00:01:10 +0530

    Added Categories for First Plan Created mail

--- likeminds_payments/subscription/utility/constants.py
+++ likeminds_payments/subscription/utility/constants.py
@@ -24,10 +24,22 @@ CREATE_COHORT_API = "{}/api/cohort/create".format(settings.CORE_SERVICE_URL)
 UPDATE_COHORT_API = "{}/api/cohort/update".format(settings.CORE_SERVICE_URL)
 FETCH_MEMBER_COHORTS_API = "{}/api/cohort/fetch_member_cohorts".format(settings.CORE_SERVICE_URL)
 PAYMENT_PAGE_BRANCH_URL = "{}/api/community/fetch_payment_page_url".format(settings.CORE_SERVICE_URL)
-COMMUNITY_FEED_CM_ONBOARDING_BRANCH_URL = "{}/api/community/fetch_feed_url_cm_onboarding".format(settings.CORE_SERVICE_URL)
+COMMUNITY_FEED_CM_ONBOARDING_BRANCH_URL = "{}/api/community/fetch_feed_url_cm_onboarding".format(
+    settings.CORE_SERVICE_URL)
 CMS_USER_NAME = 'teamGrowth'
 CMS_PASSWORD = 'TheLMGrowth@1001'
 
 TRIGGER_EVENT_CREATION_MAIL = '{}/api/notifications/send_event_creation_mail'.format(settings.CORE_SERVICE_URL)
 
 ADMIN_EMAIL = 'admin@likeminds.community'
+
+MAIL_CATEGORY_BETA = "beta"
+MAIL_CATEGORY_PROD = "prod"
+
+
+class EmailCategories:
+    CREATE_COMMUNITY = "Create community"
+
+
+class EmailSubCategories:
+    FIRST_PLAN_CREATED = "First Plan Created"

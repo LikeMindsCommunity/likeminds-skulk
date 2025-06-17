1053e52df37db5ded6bc374f6814a38b54a1f126
ankitgarg <ankit.garg@likeminds.community>
2025-06-17 21:55:36 +0530

    Added new migration file for skulk prod.

---
+++ Migrationfile-azure-skulk-prod.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+APPLICATION_ENVIRONMENT="PRODUCTION"
+APPLICATION_DOT_ENV_LOCATION="/home/apps/skulk-prod/LikeMinds-Subscription/likeminds_payments/init/settings/.env"
+APPLICATION_LOCATION="/home/apps/skulk-prod/LikeMinds-Subscription/likeminds_payments/"
+APPLICATION_MANAGE_SCRIPT_LOCATION="/home/apps/skulk-prod/LikeMinds-Subscription/likeminds_payments/manage.py"
+APPLICATION_NAME="SKULK"
+APPLICATION_REQUIREMENTS_LOCATION="/home/apps/skulk-prod/LikeMinds-Subscription/likeminds_payments/requirements.txt"
+APPLICATION_VENV_LOCATION="/home/apps/skulk-prod/skulk-prod-venv/bin/activate"
+APPICATION_ACCOUNT_NAME="likemindsstagingstorage"
+APPLICATION_CONTAINER_NAME="likeminds-staging-configs"
+APPLICATION_DOT_ENV_BLOB_NAME="skulk-beta/skulk-beta-dot-env-private"
+
+print_internal() {
+    PREFIX="\n\n **** "
+    SUFFIX=" **** \n\n"
+    STR="$PREFIX $1 $SUFFIX"
+    printf "$STR"
+}
+
+get_project_dot_env() {
+  print_internal "get and write dot env into project folder"
+  print_internal "writing file at $APPLICATION_DOT_ENV_LOCATION"
+
+  az storage blob download --account-name $APPICATION_ACCOUNT_NAME --container-name $APPLICATION_CONTAINER_NAME --name $APPLICATION_DOT_ENV_BLOB_NAME --file $APPLICATION_DOT_ENV_LOCATION
+
+  print_internal "wrote dot env into project"
+}
+
+activate_project_venv() {
+  print_internal "activating environment"
+  source "$APPLICATION_VENV_LOCATION"
+  print_internal "environment at $APPLICATION_VENV_LOCATION activate success"
+}
+
+install_project_requirements() {
+  print_internal "install project requirements"
+  pip3 install -r "$APPLICATION_REQUIREMENTS_LOCATION"
+  print_internal "project requirements install success"
+}
+
+migrate_database() {
+  print_internal "migrating database"
+
+  if [ "$APPLICATION_NAME" == "SKULK" ]
+  then
+    migrate_database_internal
+  else
+    print_internal "Unknown application $APPLICATION_NAME"
+  fi
+}
+
+migrate_database_internal() {
+  cd "$APPLICATION_LOCATION" || exit
+
+  if [ "$APPLICATION_ENVIRONMENT" == "PRODUCTION" ]
+  then
+
+    print_internal "make and perform database migrations"
+    DJANGO_SETTINGS_MODULE=init.settings.production python3 "$APPLICATION_MANAGE_SCRIPT_LOCATION" makemigrations
+    DJANGO_SETTINGS_MODULE=init.settings.production python3 "$APPLICATION_MANAGE_SCRIPT_LOCATION" migrate
+    print_internal "database migration success"
+
+  else
+    print_internal "Unknown application environment $APPLICATION_ENVIRONMENT"
+  fi
+
+  cd ~ || exit
+}
+
+deactivate_project_venv() {
+  print_internal "deactivate environment"
+  deactivate
+}
+
+migrate() {
+
+  print_internal "migrating skulk-prod database.."
+
+  get_project_dot_env
+  activate_project_venv
+  install_project_requirements
+  migrate_database
+  deactivate_project_venv
+
+  print_internal "migrated skulk-prod database.."
+}
+
+migrate
